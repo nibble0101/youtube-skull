52447a6662706fb2a4c2b431c724a0ad6e9aba2f
nibble0101 <mj.mawa@yahoo.com>
2021-06-20 13:03:21 +0300

    Add event listeners to the buttons

--- public/index.js
+++ public/index.js
@@ -1,15 +1,38 @@
 const form = document.getElementById("form");
+const videoBtn = document.getElementById("video-btn");
+const audioBtn = document.getElementById("audio-btn");
 
-function clearUrlInput(){
+let downloadVideo = true;
+
+function clearUrlInput() {
   const url = document.getElementById("url");
   url.value = "";
 }
 
+videoBtn.addEventListener("click", () => {
+  if (!videoBtn.classList.contains("active--btn")) {
+    videoBtn.classList.add("active--btn");
+    downloadVideo = true;
+  }
+  if (audioBtn.classList.contains("active--btn")) {
+    audioBtn.classList.remove("active--btn");
+  }
+});
+
+audioBtn.addEventListener("click", () => {
+  if (!audioBtn.classList.contains("active--btn")) {
+    audioBtn.classList.add("active--btn");
+    downloadVideo = false;
+  }
+  if (videoBtn.classList.contains("active--btn")) {
+    videoBtn.classList.remove("active--btn");
+  }
+});
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
   const url = document.getElementById("url");
-  if(!url.value.trim()){
-    alert("Please enter correct URL");
+  if (!url.value.trim()) {
+    alert("Please enter valid URL");
   }
 });
